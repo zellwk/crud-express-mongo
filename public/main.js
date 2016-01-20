3265b3e05fe9deaecc88af3a328343eeb6886e11
Zell Liew <zellwk@gmail.com>
2016-01-20 22:35:18 +0800

    initial commit

---
+++ public/main.js
@@ -0,0 +1,30 @@
+/* globals fetch */
+
+var update = document.getElementById('update')
+var del = document.getElementById('delete')
+update.addEventListener('click', function () {
+  fetch('quotes', {
+    method: 'put',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      'name': 'Darth Vader',
+      'quote': 'I find your lack of faith disturbing.'
+    })
+  }).then(function (response) {
+    window.location.reload()
+  })
+})
+
+del.addEventListener('click', function () {
+  fetch('quotes', {
+    method: 'delete',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      'name': 'Darth Vader'
+    })
+  }).then(function (response) {
+    window.location.reload()
+  })
+})
---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+const MongoClient = require('mongodb').MongoClient
+var db
+
+MongoClient.connect('mongodb://zellwk:zellwk@ds047955.mongolab.com:47955/star-wars-quotes', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
+})
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+  db.collection('quotes').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    res.render('index.ejs', {quotes: result})
+  })
+})
+
+app.post('/quotes', (req, res) => {
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
+
+app.put('/quotes', (req, res) => {
+  db.collection('quotes')
+  .findOneAndUpdate({}, {
+    $set: {
+      name: req.body.name,
+      quote: req.body.quote
+    }
+  }, (err, result) => {
+    if (err) return res.send(500, err)
+    res.send(result.value)
+  })
+})
+
+app.delete('/quotes', (req, res) => {
+  db.collection('quotes').findOneAndDelete({name: req.body.name}, (err, result) => {
+    if (err) return res.send(500, err)
+    res.send('A darth vadar quote got deleted')
+  })
+})
